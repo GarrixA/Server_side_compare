fbe09692e0de0f1d75ad8f3199f741cb256359ee
GarrixA <aphrogarrix10@gmail.com>
2024-04-18 13:30:23 +0200

    checking hound ci comments

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import app from "./app";
 import { mongoConnect } from "./services/mongoConnection";
 
-let x = 0
-
-
 const startServer = async () => {
 	await mongoConnect();
 
 	app.listen(8080, () => {
-		console.log("server is listening ..... 8080");	}  );
+		console.log("server is listening ..... 8080");
+	});
 };
 startServer();
